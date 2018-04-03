0d74f0a8758f49c582262a83483d734b1e274469
Alexander Spanos <alexisspa9@gmail.com>
2018-04-03 19:43:17 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,6 +15,8 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
+    @student.Save
+    redirect_to students_path(@student)
 
   end
 
